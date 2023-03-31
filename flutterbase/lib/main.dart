48daa87c516e1660e9adc9b104c786af6296a772
alberta007 <abbealpsten@gmail.com>
2023-03-31 12:44:37 +0200

    First commit

---
+++ flutterbase/lib/main.dart
@@ -0,0 +1,106 @@
+import 'dart:math';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.red,
+        ),
+        home: FutureBuilder(
+          future: _fbApp,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              print('ERROR! ${snapshot.error.toString()}');
+              return Text('Something wrong!');
+            } else if (snapshot.hasData) {
+              return HomePage();
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        )
+        //const MyHomePage(title: 'Flutter Demo Home Page'),
+        );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _barcode = '';
+  DatabaseReference _testref = FirebaseDatabase(
+          databaseURL:
+              "https://nytest-d8ca4-default-rtdb.europe-west1.firebasedatabase.app")
+      .reference()
+      .child("test");
+
+  Future<void> _scanBarcode() async {
+    String barcode = await FlutterBarcodeScanner.scanBarcode(
+      '#ff6666', // Color for the scanner overlay
+      'Cancel', // Text for the cancel button
+      true, // Whether to show the flash button
+      ScanMode.BARCODE, // What type of codes to scan for
+    );
+
+    setState(() {
+      _barcode = barcode;
+      _testref.set(barcode);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Barcode Scanner Demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Scanned barcode:',
+            ),
+            Text(
+              _barcode,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _scanBarcode,
+        tooltip: 'Scan Barcode',
+        child: Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
