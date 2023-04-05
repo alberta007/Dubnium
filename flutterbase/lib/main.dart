59cedd65ddabd31178ec693658a66545fc41d287
alberta007 <abbealpsten@gmail.com>
2023-04-06 01:12:59 +0200

    register
    La till så man kan registrera användare

--- flutterbase/lib/main.dart
+++ flutterbase/lib/main.dart
@@ -1,8 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'mainmenu.dart';
 
-void main() => runApp(MyApp());
+import 'overlays/registeroverlay.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+DatabaseReference usersRef =
+    FirebaseDatabase.instance.reference().child("users");
 
 class MyApp extends StatelessWidget {
   @override
@@ -15,44 +27,71 @@ class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green.shade800,
-      body: Center(child: Stack(
-        children: [
-
-          Container(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-              Image.asset('assets/images/Dr.Preferenec-removebg-preview.png',),
-              SizedBox(height: 50,),
-              ElevatedButton(onPressed: (){}, 
-              child: Text('Login', style: TextStyle(fontSize: 24),), style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.purple.shade500),
-                foregroundColor: MaterialStateProperty.all(Colors.grey.shade400),
-                minimumSize: MaterialStateProperty.all(Size(200,75))
-
-              ),),
-              SizedBox(height: 10,),
-              Text('Or...',style: TextStyle(fontSize: 25, color: Colors.grey.shade400),),
-              SizedBox(height: 10,),
-              ElevatedButton(onPressed: (){}, 
-              child: Text('Register', style: TextStyle(fontSize: 24),), style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.purple.shade500),
-                foregroundColor: MaterialStateProperty.all(Colors.grey.shade400),
-                minimumSize: MaterialStateProperty.all(Size(200,75))
-
-              ),),
-                            SizedBox(height: 60,),
-
-            ]),
-            
-          )
-        ],
-      )
-      ,)
-    );
-
+        backgroundColor: Colors.green.shade800,
+        body: Center(
+          child: Stack(
+            children: [
+              Container(
+                alignment: Alignment.center,
+                child:
+                    Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+                  Image.asset(
+                    'assets/images/Dr.Preferenec-removebg-preview.png',
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      MyCustomClass.openNewScreen(context);
+                    },
+                    child: Text(
+                      'Login',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.purple.shade500),
+                      foregroundColor:
+                          MaterialStateProperty.all(Colors.grey.shade400),
+                      minimumSize: MaterialStateProperty.all(Size(200, 75)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Or...',
+                    style: TextStyle(fontSize: 25, color: Colors.grey.shade400),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) => RegisterDialog());
+                    },
+                    child: Text(
+                      'Register',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.purple.shade500),
+                        foregroundColor:
+                            MaterialStateProperty.all(Colors.grey.shade400),
+                        minimumSize: MaterialStateProperty.all(Size(200, 75))),
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                ]),
+              )
+            ],
+          ),
+        ));
   }
 }
    /*  return MaterialApp(
