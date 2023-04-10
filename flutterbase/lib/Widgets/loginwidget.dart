ed90c720bdd59011d50f019fb455f68941d31d62
MarcusNyfjall <marcus.nyfjall.6742@student.uu.se>
2023-04-10 11:11:20 +0200

    nästan klar med login

--- flutterbase/lib/Widgets/loginwidget.dart
+++ flutterbase/lib/Widgets/loginwidget.dart
@@ -2,9 +2,12 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutterbase/provider/google_sign_in.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutterbase/main.dart';
 import 'package:flutterbase/Widgets/mainmenu.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 
 class LoginWidget extends StatefulWidget {
   final VoidCallback onClickSignUp;
@@ -104,7 +107,59 @@ class _LoginWidgetState extends State<LoginWidget> {
                                     style: TextStyle(
                                         decoration: TextDecoration.underline,
                                         color: Colors.purple.shade400))
-                              ]))
+                              ])),
+                          SizedBox(
+                            height: 25,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Divider(
+                                  color: Colors.white,
+                                  thickness: 1,
+                                  indent: 10,
+                                  endIndent: 10,
+                                ),
+                              ),
+                              Text(
+                                'Or',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Expanded(
+                                child: Divider(
+                                  color: Colors.white,
+                                  thickness: 1,
+                                  indent: 10,
+                                  endIndent: 10,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 33,
+                          ),
+                          ElevatedButton.icon(
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.white,
+                              onPrimary: Colors.black,
+                              minimumSize: Size(double.infinity, 50),
+                            ),
+                            icon: FaIcon(
+                              FontAwesomeIcons.google,
+                              color: Colors.blue,
+                            ),
+                            onPressed: () {
+                              final provider =
+                                  Provider.of<GoogleSignInProvider>(context,
+                                      listen: false);
+                              provider.googleLogin();
+                            },
+                            label: Text('Sign in with Google'),
+                          ),
                         ],
                       ),
                     ))
--- flutterbase/lib/main.dart
+++ flutterbase/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterbase/provider/google_sign_in.dart';
 import 'Widgets/signUpandInWidget.dart';
 import 'Widgets/mainmenu.dart';
+import 'package:provider/provider.dart';
 
 import 'package:firebase_database/firebase_database.dart';
 
@@ -15,7 +17,9 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHome());
+    return ChangeNotifierProvider(
+        create: (context) => GoogleSignInProvider(),
+        child: MaterialApp(home: MyHome()));
   }
 }
 
